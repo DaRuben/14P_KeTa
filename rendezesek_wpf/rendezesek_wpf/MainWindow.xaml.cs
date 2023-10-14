b667c3a40c22167ba3892f5f117833cea3301170
DaRuben <122792356+DaRuben@users.noreply.github.com>
2023-10-14 12:47:27 +0200

    Add files via upload

---
+++ rendezesek_wpf/rendezesek_wpf/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace rendezesek_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int[] A_tomb;
+        List<int> randomSzamok, rendezettSzamok;
+        static Random R;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void egyszeru_Click(object sender, RoutedEventArgs e)
+        {
+            EgyszeruCseresRendezes egysz = new EgyszeruCseresRendezes();
+            egysz.Show();
+        }
+
+        private void buborek_Click(object sender, RoutedEventArgs e)
+        {
+            BuborekRendezes buborek = new BuborekRendezes();
+            buborek.Show();
+        }
+
+        private void minMax_Click(object sender, RoutedEventArgs e)
+        {
+            MinMaxRendezes minMax = new MinMaxRendezes();
+            minMax.Show();
+        }
+
+        private void shell_Click(object sender, RoutedEventArgs e)
+        {
+            ShellBeszuroRendezes shell = new ShellBeszuroRendezes();
+            shell.Show();
+        }
+        
+
+    }
+}
---
+++ rendezesek_wpf/rendezesek_wpf/MinMaxRendezes.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace rendezesek_wpf
+{
+    /// <summary>
+    /// Interaction logic for MinMaxRendezes.xaml
+    /// </summary>
+    public partial class MinMaxRendezes : Window
+    {
+        List<int> randomSzamok, rendezettSzamok;
+        static Random R;
+
+        public MinMaxRendezes()
+        {
+            InitializeComponent();
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            int db = Int32.Parse(t1.Text);
+            R = new Random();
+            int szam;
+            randomSzamok = new List<int>();
+            for (int i = 0; i < db; i++)
+            {
+                szam = R.Next(101);
+                lb1.Items.Add(szam);
+                randomSzamok.Add(szam);
+            }
+        }
+
+        private void egyszeru_Click(object sender, RoutedEventArgs e)
+        {
+            EgyszeruCseresRendezes egysz = new EgyszeruCseresRendezes();
+            egysz.Show();
+        }
+
+        private void buborek_Click(object sender, RoutedEventArgs e)
+        {
+            BuborekRendezes buborek = new BuborekRendezes();
+            buborek.Show();
+        }
+
+        private void minMax_Click(object sender, RoutedEventArgs e)
+        {
+            MinMaxRendezes minMax = new MinMaxRendezes();
+            minMax.Show();
+        }
+        private void b2_Click(object sender, RoutedEventArgs e)
+        {
+            int[] randomSzamokTomb = randomSzamok.ToArray();
+            int[] minMax = MinMax(randomSzamokTomb);
+            rendezettSzamok = new List<int>();
+
+            int szam = 0;
+            for (int i = 0; i < minMax.Length; i++)
+            {
+                szam = minMax[i];
+                lb2.Items.Add(szam);
+                rendezettSzamok.Add(szam);
+            }
+        }
+
+
+        private void shell_Click(object sender, RoutedEventArgs e)
+        {
+            ShellBeszuroRendezes shell = new ShellBeszuroRendezes();
+            shell.Show();
+        }
+
+        public int[] Egyszeru_Cseres(int[] A_tomb)
+        {
+            int csere = 0;
+            for (int i = 0; i < A_tomb.Length - 1; i++)
+            {
+                for (int j = i + 1; j < A_tomb.Length; j++)
+                {
+                    if (A_tomb[i] > A_tomb[j])
+                    {
+                        csere = A_tomb[i];
+                        A_tomb[i] = A_tomb[j];
+                        A_tomb[j] = csere;
+                    }
+                }
+            }
+            return A_tomb;
+        }
+
+        public int[] Buborek(int[] A_tomb)
+        {
+            int csere = 0;
+            for (int i = A_tomb.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (A_tomb[j] > A_tomb[j + 1])
+                    {
+                        csere = A_tomb[j];
+                        A_tomb[j] = A_tomb[j + 1];
+                        A_tomb[j + 1] = csere;
+                    }
+                }
+            }
+            return A_tomb;
+        }
+
+        public int[] MinMax(int[] A_tomb)
+        {
+            int csere = 0, akt_index = 0;
+            for (int i = 0; i < A_tomb.Length - 1; i++)
+            {
+                akt_index = i;
+                for (int j = i + 1; j < A_tomb.Length; j++)
+                {
+                    if (A_tomb[akt_index] > A_tomb[j]) akt_index = j;
+                }
+                csere = A_tomb[i];
+                A_tomb[i] = A_tomb[akt_index];
+                A_tomb[akt_index] = csere;
+            }
+            return A_tomb;
+        }
+
+        public int[] Shell(int[] A_tomb)
+        {
+            int csere = 0, tavolsag = A_tomb.Length / 3, j = 0;
+            while (tavolsag > 0)
+            {
+                for (int i = 0; i < A_tomb.Length; i++)
+                {
+                    j = i;
+                    csere = A_tomb[i];
+                    while (j >= tavolsag && A_tomb[j - tavolsag] > csere)
+                    {
+                        A_tomb[j] = A_tomb[j - tavolsag];
+                        j = j - tavolsag;
+                    }
+                    A_tomb[j] = csere;
+                }
+                if (tavolsag / 2 != 0) tavolsag = tavolsag / 2;
+                else if (tavolsag == 1) tavolsag = 0;
+                else tavolsag = 1;
+            }
+            return A_tomb;
+        }
+
+    }
+}
