9e923a9ec447a3f7e96be49a0bfb1b22793e9ac1
DaRuben <122792356+DaRuben@users.noreply.github.com>
2023-11-12 10:50:34 +0100

    Add files via upload

---
+++ emlosok_forms/emlosok_forms/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace emlosok_forms
+{
+    public partial class Form1 : Form
+    {
+        List<string> kepek;
+        int mozgas;
+
+        PictureBox kep;
+        Button bal, jobb, kepAdatai;
+
+        public Form1()
+        {
+            InitializeComponent();
+            kepBetolt();
+            kepMegjelenit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = "Emlősök";
+            Size = new Size(550, 350);
+
+            bal = new Button()
+            {
+                Parent = this,
+                Location = new Point(20, 270),
+                Size = new Size(100, 30),
+                Text = "<",
+            };
+            jobb = new Button()
+            {
+                Parent = this,
+                Location = new Point(420, 270),
+                Size = new Size(100, 30),
+                Text = ">",
+            };
+            kepAdatai = new Button()
+            {
+                Parent = this,
+                Location = new Point(220, 270),
+                Size = new Size(100, 30),
+                Text = "Tovább",
+            };
+
+            bal.Click += Bal_Click;
+            jobb.Click += Jobb_Click;
+            kepAdatai.Click += KepAdatai_Click;
+        }
+
+        private void KepAdatai_Click(object sender, EventArgs e)
+        {
+            kep_adatai kep = new kep_adatai();
+            kep.ShowDialog();
+        }
+
+        private void Bal_Click(object sender, EventArgs e)
+        {
+            mozgas--;
+            if (mozgas >= kepek.Count)
+            {
+                mozgas = -1;
+            }
+            else
+            {
+                kep.Image = Image.FromFile(kepek[mozgas]);
+            }
+        }
+
+        private void Jobb_Click(object sender, EventArgs e)
+        {
+            mozgas++;
+            if (mozgas >= kepek.Count)
+            {
+                mozgas = -1;
+            }
+            else
+            {
+                kep.Image = Image.FromFile(kepek[mozgas]);
+            }
+        }
+
+        private void kepBetolt()
+        {
+            string utvonal = @"D:\boronkay\14p\keta\emlosok\emlosok_forms\emlosok_forms\bin\Debug\képek"; 
+            kepek = new List<string>();
+            
+            /* string utvonalKiterj = "";
+            if (utvonal == "ember")
+            {
+                utvonalKiterj = utvonal + "*.png";
+            }*/
+
+            kepek = Directory.GetFiles(utvonal, "*.jpg").ToList();
+        }
+
+        private void kepMegjelenit()
+        {
+            kep = new PictureBox()
+            {
+                Parent = this,
+                Location = new Point(20, 10),
+                Size = new Size(500, 250),
+                SizeMode = PictureBoxSizeMode.Zoom,
+            };
+            kep.Image = Image.FromFile(kepek[mozgas]);
+        }
+
+    }
+}
---
+++ emlosok_forms/emlosok_forms/kep_adatai.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.LinkLabel;
+using System.Net.NetworkInformation;
+
+
+namespace emlosok_forms
+{
+    public partial class kep_adatai : Form
+    {
+        List<string[]> txt;
+        List<string> fajok;
+        string kivFaj;
+
+        ComboBox CBfajok;
+        Label fajAdatok, fajlnev, eleresi_ut, meret, letrehozas_ideje;
+        PictureBox fajKep;
+
+        public kep_adatai()
+        {
+            InitializeComponent();
+        }
+
+        private void kep_adatai_Load(object sender, EventArgs e)
+        {
+            Text = "Kiválasztott emlős adatai";
+            Size = new Size(900, 500);
+
+            beolvasEsFeltolt();
+        }
+
+        private void CBfajok_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            fajlnev = new Label()
+            {
+                Parent = this,
+                Location = new Point(20, 80),
+                AutoSize = true,
+            };
+            eleresi_ut = new Label()
+            {
+                Parent = this,
+                Location = new Point(20, 400),
+                AutoSize = true,
+            };
+            meret = new Label()
+            {
+                Parent = this,
+                Location = new Point(20, 110),
+                AutoSize = true,
+            };
+            letrehozas_ideje = new Label()
+            {
+                Parent = this,
+                Location = new Point(20, 140),
+                AutoSize = true,
+            };
+
+            fajAdatok = new Label()
+            {
+                Parent = this,
+                Location = new Point(20, 430),
+                AutoSize = true,
+            };
+            fajKep = new PictureBox()
+            {
+                Parent = this,
+                Location = new Point(310, 20),
+                Size = new Size(400, 300),
+                SizeMode = PictureBoxSizeMode.Zoom,
+            };
+
+            kivFaj = CBfajok.SelectedValue.ToString();
+            string[] kivalasztottSor = null;
+
+            for (int i = 0; i < txt.Count; i++)
+            {
+                if (txt[i][4] == kivFaj)
+                {
+                    kivalasztottSor = txt[i];
+                }
+            }
+
+            if (kivalasztottSor != null)
+            {
+                fajAdatok.Text = string.Join(", ", kivalasztottSor);
+            }
+            else
+            {
+                fajAdatok.Text = "Nincs találat.";
+            }
+            fajKep.Image = Image.FromFile(@"képek\" + kivFaj + ".jpg");
+
+            Text = kivFaj+" adatai";
+
+            fajlnev.Text = kivFaj + ".jpg";
+            eleresi_ut.Text = @"D:\boronkay\14p\keta\emlosok\emlosok_forms\emlosok_forms\bin\Debug\képek\" + kivFaj + ".jpg";
+            
+            long kepMeret = new FileInfo(@"képek\" + kivFaj + ".jpg").Length;
+            meret.Text = kepMeret/1024 + " KB";
+            
+            DateTime ido = new FileInfo(@"képek\" + kivFaj + ".jpg").CreationTime;
+            letrehozas_ideje.Text = ido.ToString();
+        }
+        private void beolvasEsFeltolt()
+        {
+            txt = new List<string[]>();
+            fajok = new List<string>();
+            string[] be = File.ReadAllLines("fajok.txt", Encoding.Default);
+
+            for (int i = 0; i < be.Length; i++)
+            {
+                string[] ertekek = be[i].Split('\t');
+                txt.Add(ertekek);
+            }
+
+            for (int i = 0; i < txt.Count; i++)
+            {
+                if (i != 0)
+                {
+                    fajok.Add(txt[i][4]);
+                }
+                
+            }
+            CBfajok = new ComboBox()
+            {
+                Parent = this,
+                Location = new Point(20, 20),
+                Size = new Size(200, 100),
+            };
+            
+            
+            CBfajok.DataSource= fajok;
+            CBfajok.SelectedIndexChanged += CBfajok_SelectedIndexChanged;
+        }
+
+    }
+}
