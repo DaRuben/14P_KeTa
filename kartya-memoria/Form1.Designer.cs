77d73f89c49197d969587a7c756b8ab6da82769c
DaRuben <122792356+DaRuben@users.noreply.github.com>
2024-01-15 20:14:06 +0100

    Add files via upload

---
+++ kartya-memoria/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace kartya_memoria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.x4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x6ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x6ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.x8ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.x4ToolStripMenuItem,
+            this.x6ToolStripMenuItem,
+            this.x6ToolStripMenuItem1,
+            this.x8ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(484, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // x4ToolStripMenuItem
+            // 
+            this.x4ToolStripMenuItem.Name = "x4ToolStripMenuItem";
+            this.x4ToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.x4ToolStripMenuItem.Text = "4x4";
+            this.x4ToolStripMenuItem.Click += new System.EventHandler(this.x4ToolStripMenuItem_Click);
+            // 
+            // x6ToolStripMenuItem
+            // 
+            this.x6ToolStripMenuItem.Name = "x6ToolStripMenuItem";
+            this.x6ToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.x6ToolStripMenuItem.Text = "4x6";
+            this.x6ToolStripMenuItem.Click += new System.EventHandler(this.x6ToolStripMenuItem_Click);
+            // 
+            // x6ToolStripMenuItem1
+            // 
+            this.x6ToolStripMenuItem1.Name = "x6ToolStripMenuItem1";
+            this.x6ToolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.x6ToolStripMenuItem1.Text = "6x6";
+            this.x6ToolStripMenuItem1.Click += new System.EventHandler(this.x6ToolStripMenuItem1_Click);
+            // 
+            // x8ToolStripMenuItem
+            // 
+            this.x8ToolStripMenuItem.Name = "x8ToolStripMenuItem";
+            this.x8ToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.x8ToolStripMenuItem.Text = "6x8";
+            this.x8ToolStripMenuItem.Click += new System.EventHandler(this.x8ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 561);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem x4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x6ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x6ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem x8ToolStripMenuItem;
+    }
+}
+
---
+++ kartya-memoria/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace kartya_memoria
+{
+    public partial class Form1 : Form
+    {
+        Label[] kartyak;
+        int hossz, szelesseg;
+        string felirat1 = "";
+        string felirat2 = "";
+        int ellSzamlalo = 0;
+        int parSzamlalo;
+        Label par;
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            par = new Label()
+            {
+                Parent = this,
+                Text = "Megtalált párok: ",
+                AutoSize = true,
+                Size = new Size(50, 20),
+                Location = new Point(10, 25),
+            };
+        }
+        private void vezerlokMegjelenit(int hossz, int szelesseg)
+        {
+            int szamlalo = 0;
+            int osszes = hossz * szelesseg;
+
+            kartyak = new Label[osszes];
+            List<int> feliratok = new List<int>();
+            List<int> feliratok1 = new List<int>();
+            List<int> feliratok2 = new List<int>();
+            for (int i = 1; i < (osszes+2)/2; i++)
+            {
+                feliratok1.Add(i);
+                feliratok2.Add(i);
+            }
+            feliratok.AddRange(feliratok1);
+            feliratok.AddRange(feliratok2);
+            listaKever(feliratok);
+
+            for (int i = 0; i < hossz; i++)
+            {
+                for (int j = 0; j < szelesseg; j++)
+                {
+                    kartyak[i] = new Label()
+                    {
+                        Parent = this,
+                        Text = feliratok[szamlalo].ToString(),
+                        BorderStyle = BorderStyle.FixedSingle,
+                        Size = new Size(50, 50),
+                        Location = new Point(i * 55 + 10, j * 55 + 70),
+                    };
+                    szamlalo++;
+
+                    kartyak[i].Click += Ell;
+                }
+            }
+        }
+        private void listaKever(List<int> t)
+        {
+            for (int i = t.Count - 1; i > 0; i--)
+            {
+                int j = r.Next(0, i + 1);
+
+                int temp = t[i];
+                t[i] = t[j];
+                t[j] = temp;
+            }
+        }
+
+        private void Ell(object sender, EventArgs e)
+        {
+            ellSzamlalo++;
+            Label label1 = new Label();
+            Label label2 = new Label();
+            Label label = sender as Label;
+
+            if (ellSzamlalo == 1)
+            {
+                label1 = label;
+                felirat1 = label1.Text.ToString();
+                label1.BackColor = Color.LightBlue;
+            }
+            else if (ellSzamlalo == 2)
+            {
+                label2 = label;
+                felirat2 = label2.Text.ToString();
+                label2.BackColor = Color.LightBlue;
+
+                if (felirat1 == felirat2)
+                {
+                    ellSzamlalo = 0;
+                    parSzamlalo++;
+                    label1.BackColor = Color.Green;
+                    label2.BackColor = Color.Green;
+                    par.Text = "Megtalált párok: " + parSzamlalo;
+
+                    if (parSzamlalo == 8)
+                    {
+                        MessageBox.Show("WIN!");
+                    }
+                }
+                else
+                {
+                    ellSzamlalo = 0;
+                    label1.BackColor = Color.White;
+                    label2.BackColor = Color.White;
+                }
+            }
+        }
+        private void x4ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            hossz = 4;
+            szelesseg = 4;
+            vezerlokMegjelenit(hossz, szelesseg);
+        }
+        private void x6ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            hossz = 4;
+            szelesseg = 6;
+            vezerlokMegjelenit(hossz, szelesseg);
+        }
+        private void x6ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            hossz = 6;
+            szelesseg = 6;
+            vezerlokMegjelenit(hossz, szelesseg);
+        }
+        private void x8ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            hossz = 6;
+            szelesseg = 8;
+            vezerlokMegjelenit(hossz, szelesseg);
+        }
+
+
+    }
+}
